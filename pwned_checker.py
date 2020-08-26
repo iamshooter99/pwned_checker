4aa247c5405968257a7e983155f02893eb81cf7e
Tarun Garg <45597781+tarungarg1999@users.noreply.github.com>
2020-08-26 15:57:47 +0530

    Update pwned_checker.py

--- pwned_checker.py
+++ pwned_checker.py
@@ -6,10 +6,16 @@ parser.add_argument("-v",'--version',action='version',version='%(prog)s 1.0')
 parser.add_argument("-u","--username",type=str,help='username to be checking')
 parser.add_argument("-p",'--password',type=str,help='password to be checking')
 parser.add_argument("-e",'--email',type=str,help='email to be checking')
+parser.add_argument("-w","--wordlists",type=str,help='wordlists to be checking')
 args=parser.parse_args()
 if args.username==None and args.password==None and args.email==None:
 	print("Please specify valid arguments!!!")
 	exit(1)
+
+if args.wordlists:
+	wordlist=args.wordlists
+else:
+	wordlist="/usr/share/wordlists/rockyou.txt"
 print("++++++ Welcome to pwned checker +++++++++")
 print("checking........")
 if args.password:
@@ -24,7 +30,7 @@ if args.password:
 
 if args.username:
 	args.username+='\n'
-	with open('/usr/share/wordlists/rockyou.txt','r') as f:
+	with open(wordlist,'r') as f:
 		for line in f:
 			if line==args.username:
 				print("Oh no - Pwned!!!")
